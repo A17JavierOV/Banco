19345c421052d299394ba4c90be597ed694e1170
XabySCQ <XabySCQ@192.168.0.13>
2018-05-07 12:46:46 +0200

    Modificaciones actualizadas con las clases entregadas el viernes. Agregado TryCatch a la clase main para solucionar un error Implementadas la creación y comprobación de DNI y CCC, corregidos los HashSet en Cuenta y Cliente

--- src/banco/Banco.java
+++ src/banco/Banco.java
@@ -1,13 +1,13 @@
 package banco;
 import java.util.Scanner;
 import cuenta.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import utilidadesbanco.*;
+import static utilidadesbanco.UtilidadesBanco.validarCCC;
+import static utilidadesbanco.UtilidadesBanco.validarIBAN;
 
 public class Banco {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CCCException {
         
         Scanner sc = new Scanner(System.in);
         int opcion;
@@ -24,6 +24,8 @@ public class Banco {
         while(opcion!=0){
             
             if(opcion==1){
+                
+                int seleccion;
 
                 System.out.println("UTILIDADES BANCO");
                 System.out.println("[1]. Calcular CCC");
@@ -31,26 +33,62 @@ public class Banco {
                 System.out.println("[0]. Salir");
 
                 System.out.println("Seleccionar una opción: ");
-                opcion = sc.nextInt();
+                seleccion = sc.nextInt();
+                
+                while(seleccion!=0){
+                
+                    if(seleccion==1){
+                        String ccc;
+                        System.out.println("Inserte CCC: ");
+                        ccc = sc.next();
+                        try{ 
+                            validarCCC(ccc);
+                            System.out.println("CCC válido.");
+                            break;
+                        }catch(CCCException ex){
+                            System.out.println(ex.getMessage());
+                        }
+                    }else if(seleccion==2){
+                        String iban;
+                        System.out.println("Inserte IBAN: ");
+                        iban = sc.next();
+                        try{ 
+                            validarIBAN(iban);
+                            System.out.println("IBAN válido.");
+                            break;
+                        }catch(IbanException ibanex){
+                            System.out.println("El IBAN que estás intentando insertar no es válido.");
+                        }
+
+                    }else if(seleccion==0){
+                        System.out.println("Gracias por usar nuestros servicios.");
+                    }
+                }
 
             }else if(opcion==2){
                 
                 try {
-                    String entidad, oficina, cliente;
+                    String dni, nombre, entidad, oficina, cliente;
+                                        
                     System.out.println("Introduzca los datos: ");
+                    System.out.println("Nombre del titular: ");
+                    nombre = sc.next();
+                    System.out.println("DNI del titular: ");
+                    dni = sc.next();
+                    Cliente datosCliente = new Cliente(dni, nombre);
                     System.out.println("Entidad: ");
-                    entidad = sc.nextLine();
+                    entidad = sc.next();
                     System.out.println("Oficina: ");
-                    oficina = sc.nextLine();
-                    System.out.println("Cliente: ");
-                    cliente = sc.nextLine();
+                    oficina = sc.next();
+                    System.out.println("Número de cuenta cliente: ");
+                    cliente = sc.next();
                     System.out.println("Su CCC: " + utilidadesbanco.UtilidadesBanco.calcular_CCC(entidad, oficina, cliente));
                 } catch (CCCException ex) {
-                    Logger.getLogger(Banco.class.getName()).log(Level.SEVERE, null, ex);
+                    System.out.println("El valor que estás intentando introducir no es válido");
                 }
-
             }else{
-
+                System.out.println("Gracias por usar nuestros servicios.");
+                break;
             }
         }
     }
